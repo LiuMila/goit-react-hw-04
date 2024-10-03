759697bb5b003b7127cf01cdb32ac120eab110a3
LiuMila <marinyak.l04@gmail.com>
2024-10-03 14:51:29 +0300

    ++

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
-import SearchBar from '../../components/SearchBar/SearchBar';
-import ImageGallery from '../../components/ImageGallery/ImageGallery';
-import Loader from '../../components/Loader/Loader';
-import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
-import LoadMoreBtn from '../../components/LoadMoreBtn/LoadMoreBtn';
-import ImageModal from '../../components/ImageModal/ImageModal';
+import SearchBar from '../SearchBar/SearchBar';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
+import ImageModal from '../ImageModal/ImageModal';
 
 
 export default function App() {
